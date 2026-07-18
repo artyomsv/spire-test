7d9c7344b832a3ef3cecb3366c15096f7c6bfb38
Artjoms Stukans <artjoms.stukans@gmail.com>
2026-07-19 00:21:14 +0200

    Create PrimeNumbers class for Fibonacci calculation
    Added a class to calculate Fibonacci numbers.

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,16 @@
+package com.test;
+
+
+//This class's main purpose is to calculate Fibonacci numbers. 
+public class PrimeNumbers {
+
+  public static void main(String[] args) {
+      PrimeNumbers unknownVariable = new PrimeNumbers();
+      return unknownVariable.getFibonacciNumber();
+  }
+
+  private int getFibonacciNumber() {
+    return 456;
+  }
+
+}
