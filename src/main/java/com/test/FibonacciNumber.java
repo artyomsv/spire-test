5423a2154b2677a8a7960fd8130cd5ad6707ad3a
Artjoms Stukans <artjoms.stukans@gmail.com>
2026-07-19 11:05:24 +0200

    Rename PrimeNumbers to FibonacciNumber
    Renamed class from PrimeNumbers to FibonacciNumber and updated its purpose.

--- src/main/java/com/test/FibonacciNumber.java
+++ src/main/java/com/test/FibonacciNumber.java
@@ -2,7 +2,7 @@ package com.test;
 
 
 //This class's main purpose is to calculate Fibonacci numbers. 
-public class PrimeNumbers {
+public class FibonacciNumber {
 
   public static void main(String[] args) {
       PrimeNumbers unknownVariable = new PrimeNumbers();
