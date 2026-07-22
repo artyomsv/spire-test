6ffee7faf488c96ce085e717133960379f945dbd
Artjoms Stukans <artjoms.stukans@gmail.com>
2026-07-22 22:45:51 +0200

    Create FibonacciNumber class for Fibonacci calculation
    Implement FibonacciNumber class to calculate Fibonacci numbers.

---
+++ src/main/java/com/testt/Fibonaccci.java
@@ -0,0 +1,15 @@
+package com.test;
+
+//This class's main purpose is to calculate Fibonacci numbers. 
+public class FibonacciNumber {
+
+  public static void main(String[] args) {
+      FibonacciNumber unknownVariable = new FibonacciNumber();
+      System.out.println(unknownVariable.getFibonacciNumber());
+  }
+
+  private int getFibonacciNumber() {
+    return 456;
+  }
+
+}
