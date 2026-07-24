1255abd01f9f169d7faf3e109cd9a10b870d4c34
Artjoms Stukans <artjoms.stukans@gmail.com>
2026-07-25 01:56:47 +0200

    Create TicTacToe class with main method
    Initial implementation of the TicTacToe class with main method and basic structure.

---
+++ src/main/java/com/stukans/TicTacToe.java
@@ -0,0 +1,16 @@
+public class Calculator {
+
+  public static void main(String[] args) {
+    Game game = new Game();  
+    game.play();
+  }
+
+  private static void move(int x, int y) {
+    make move
+  }
+
+  private statis void win() {
+    System.out.println("User 1 won!")
+  }
+
+}
