629092747f8b47cb974a7b8a007d724ca628910b
Artjoms Stukans <artjoms.stukans@gmail.com>
2026-07-26 21:18:29 +0200

    test commit (#14)
    * test commit
    
    * Fix typo in win method declaration
    
    * Add static variable and fix syntax error in TicTacToe
    
    Added a static variable and fixed a missing semicolon.
    
    * Fix win method call in TicTacToe main method
    
    * Change VARIABLE from Long to String type
    
    * move package
    
    * fix class name

---
+++ src/main/java/com/codereview/TicTacToe.java
@@ -0,0 +1,29 @@
+package com.codereview;
+
+public class TicTacToe {
+
+  private static String VARIABLE = "Variable";
+
+  public static void main(String[] args) {
+    Game game = new Game();  
+    game.play();
+    game.move(1, 2);
+    win();
+  }
+
+    public static void win() {
+      System.out.println("User 1 won!");
+    }
+
+  public static class Game {    
+    
+    public void play(){
+      System.out.println("Game started");
+    }
+
+    public void move(int x, int y) {
+      System.out.println("User made move to x:" + x + " y:" + y);
+    }
+  }
+  
+}
