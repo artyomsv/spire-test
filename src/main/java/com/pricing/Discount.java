a3aab7c242354123d43fc3cfb9b668faf7005456
code-spire-beaver <code-spire-beaver@factory.invalid>
2026-09-10 19:54:18 +0000

    autosave: work in progress

--- src/main/java/com/pricing/Discount.java
+++ src/main/java/com/pricing/Discount.java
@@ -14,6 +14,6 @@ public final class Discount {
      * @return what the customer pays, in millicents
      */
     public static long afterDiscount(long chargeMillicents, int percent) {
-        return chargeMillicents * percent / 100;
+        return chargeMillicents - (chargeMillicents * percent / 100);
     }
 }
